fc0b57ba3debb31782600cc986e9b2a233e1e157
David Mohl <me@dave.cx>
2014-07-04 05:31:44 +0900

    compiling opp on first start if not existent

--- modules/opp/init.zsh
+++ modules/opp/init.zsh
@@ -5,8 +5,16 @@
 
 # Thank you very much, Bram Moolenaar!
 # I want to use the Vim's text-objects in zsh.
-pmodload 'editor'
-
-source "${0:h}/external/opp.zsh"
-source "${0:h}/external/opp/surround.zsh"
-source "${0:h}/external/opp/textobj-between.zsh"
+if [[ ! -f ${0:h}/functions/opp.zwc ]] ||
+   [[ ! -f ${0:h}/functions/opp-install.zwc ]]; then
+  (
+    . "${0:h}/external/opp.zsh"
+    . "${0:h}/external/opp/surround.zsh"
+    . "${0:h}/external/opp/textobj-between.zsh"
+    opp-zcompile "${0:h}/external/opp.zsh" ${0:h}/functions > /dev/null
+  )
+  fpath+=${0:h}/functions > /dev/null
+  . ${0:h}/functions/opp-install
+  autoload opp
+fi
+opp-install
